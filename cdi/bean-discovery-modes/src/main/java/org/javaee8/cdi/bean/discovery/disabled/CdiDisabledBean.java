150e180d4c0f63ba532c0d4e0b7a6ff19f03d8c5
Fryze14 <56521241+Fryze14@users.noreply.github.com>
2021-02-08 09:29:51 +0100

    Primer commit del repositorio 2

--- cdi/bean-discovery-modes/src/main/java/org/javaee8/cdi/bean/discovery/disabled/CdiDisabledBean.java
+++ cdi/bean-discovery-modes/src/main/java/org/javaee8/cdi/bean/discovery/disabled/CdiDisabledBean.java
@@ -3,6 +3,6 @@ package org.javaee8.cdi.bean.discovery.disabled;
 public class CdiDisabledBean {
 	
 	int Prueba2 = 1;
-	String mensaje = "Segundo commit de la semana 2";
+	String mensaje = "Hola esta es una prueba del reporitorio 2";
 
 }
