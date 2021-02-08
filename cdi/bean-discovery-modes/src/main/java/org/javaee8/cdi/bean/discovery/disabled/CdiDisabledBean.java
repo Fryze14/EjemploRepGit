f5b4a961eb2a0533c7b54ee02228c22ccc1a90ed
Fryze14 <56521241+Fryze14@users.noreply.github.com>
2021-02-08 09:13:41 +0100

    Primer commit de la semana

--- cdi/bean-discovery-modes/src/main/java/org/javaee8/cdi/bean/discovery/disabled/CdiDisabledBean.java
+++ cdi/bean-discovery-modes/src/main/java/org/javaee8/cdi/bean/discovery/disabled/CdiDisabledBean.java
@@ -3,5 +3,5 @@ package org.javaee8.cdi.bean.discovery.disabled;
 public class CdiDisabledBean {
 
 	int Prueba = 2;
-	String Mensaje = "Hola que tals";
+	String Mensaje = "Hola esta es una prueba de la semana 2";
 }
