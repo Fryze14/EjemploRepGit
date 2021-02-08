c7fa2a6101cab2a226f549141117928f0eb53163
Fryze14 <56521241+Fryze14@users.noreply.github.com>
2021-02-08 09:37:55 +0100

    Commit simple del workspaces 1

--- cdi/bean-discovery-modes/src/main/java/org/javaee8/cdi/bean/discovery/disabled/CdiDisabledBean.java
+++ cdi/bean-discovery-modes/src/main/java/org/javaee8/cdi/bean/discovery/disabled/CdiDisabledBean.java
@@ -2,9 +2,6 @@ package org.javaee8.cdi.bean.discovery.disabled;
 
 public class CdiDisabledBean {
 	
-	int Prueba2 = 1;
-	String mensaje = "Hola esta es una prueba del reporitorio 2";
-
-	int Prueba = 2;
-	String Mensaje = "Hola esta es una prueba del repositorio 1";
+	int Prueba = 1;
+	String mensaje = "Hola";
 }
